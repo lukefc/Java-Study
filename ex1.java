b5dbfaac470cd726fd90a64ed1717c84a6e49e47
Lucas Fernando de Campos <cromlucas@psididium.psilidium>
2018-08-30 19:21:24 -0300

    Java exer

---
+++ ex1.java
@@ -0,0 +1,14 @@
+
+sckage flu;
+
+Exercício 1 -
+public class Campeao {
+	public static void main(String[] args) {
+		int fluCampeao[]; 
+		fluCampeao = new int[100];
+		for (int i = 0; i < fluCampeao.length; i++) {
+			fluCampeao[i] = i; 
+			System.out.println(fluCampeao[i]);
+		}
+	}
+}
